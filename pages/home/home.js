178ffc1ac214cfe3816bd8638195377b9cf55a21
GS233 <609009680@qq.com>
2020-04-11 13:52:16 +0800

    Initial Commit

---
+++ pages/home/home.js
@@ -0,0 +1,62 @@
+// pages/home/home.js
+Page({
+
+  data: {
+    dormList: [
+      // 第一个swiper-item
+      [{
+        id: "第一个",
+        image: "https://www.easyicon.net/api/resizeApi.php?id=1221973&size=64"
+        //连云端再改地址
+      }],
+      // 第二个swiper-item
+      [{
+        id: "第二个",
+        image: "https://www.easyicon.net/api/resizeApi.php?id=1221973&size=64"
+      }
+      ],
+      [{
+        id: "第三个",
+        image: "https://www.easyicon.net/api/resizeApi.php?id=1221973&size=64"
+      }
+      ],
+      [{
+        id: "第四个",
+        image: "https://www.easyicon.net/api/resizeApi.php?id=1221973&size=64"
+      }
+      ],
+    ],
+  },
+
+
+
+  onShareAppMessage() {
+    return {
+      title: 'swiper',
+      path: 'page/component/pages/swiper/swiper'
+    }
+  },
+  changeIndicatorDots() {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+
+  changeAutoplay() {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+
+  intervalChange(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+
+  durationChange(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  }
+})
